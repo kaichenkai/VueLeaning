78c78f6094642029d6ff1a8863b94905f18b7cc4
chenkai_dell_pc <940799064@qq.com>
2020-04-22 22:35:06 +0800

    1. heroes 增加
    Signed-off-by: chenkai_dell_pc <940799064@qq.com>

--- codes/heroes-demo/src/components/list/add.vue
+++ codes/heroes-demo/src/components/list/add.vue
@@ -3,31 +3,59 @@
         <h2 class="sub-header">Add Hero</h2>
         <form action="">
             <div class=" form-group">
-                <label for="exampleInputEmail1">Email address</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+                <label for="name">姓名</label>
+                <input type="text" class="form-control" id="name" placeholder="请输入姓名" v-model="heroes.name">
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                <label for="sex">性别</label>
+                <input type="text" class="form-control" id="sex" placeholder="请输入性别" v-model="heroes.sex">
             </div>
             <div class="form-group">
-                <label for="exampleInputFile">File input</label>
-                <input type="file" id="exampleInputFile">
-                <p class="help-block">Example block-level help text here</p>
+                <label for="age">年龄</label>
+                <input type="number" class="form-control" id="age" placeholder="请输入年龄" v-model="heroes.age">
             </div>
-            <div class="checkbox">
-                <label>
-                    <input type="checkbox">Check me out
-                </label>
-            </div>
-            <button type="submit" class="btn btn-success">Submit</button>
+            <button type="submit" class="btn btn-success" @click="addHeroes()">Submit</button>
         </form>
     </div>
 </template>
 
 <script>
+    import axios from "axios";
     export default {
-        name: "add"
+        name: "add",
+        data(){
+            return {
+                heroes: {
+                    name: "",
+                    sex: "",
+                    age: ""
+                }
+            }
+        },
+        methods: {
+            addHeroes() {
+                if (!this.heroes.name) {
+                    alert("请输入姓名")
+                }
+                // axios.post("http://localhost:3000/heroes",{name: this.heroes.name, sex:this.heroes.sex, age: this.heroes.age})
+                axios.post("http://localhost:3000/heroes",this.heroes)
+                    .then((response) => {
+                        let {status} = response;
+                        if (status === 201) {
+                            alert("添加成功")
+                        }
+                        this.heroes = {
+                            name: "",
+                            sex: "",
+                            age: ""
+                        };
+
+                        this.$router.push({name: "list"})
+                    }).then((error) => {
+                        console(error)
+                })
+            }
+        }
     }
 </script>
 
