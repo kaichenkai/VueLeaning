21af4ea140777ac4a1a06b56890a992f810bc7a8
chenkai <940799064@qq.com>
2020-04-30 17:19:08 +0800

    数据发布, 折线图完成

--- codes/access_demo/src/components/publish/lineChart.vue
+++ codes/access_demo/src/components/publish/lineChart.vue
@@ -4,50 +4,108 @@
     export default {
         name: "lineChart",
         extends: Line,
+        props: {
+            startDate: {type: String, required: false}
+        },
         data() {
             return {
-                publishReceive: null,
-                publishSuccess: null
+                publishReceiveLine: null,
+                publishSuccessLine: null
             }
         },
         mounted() {
-            this.publishReceive = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
-            this.publishSuccess = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+            this.drawLine();
+        },
+         methods: {
+            async drawLine(startDate="") {
+                let params = {};
+                if (startDate) {
+                    params = {startDate: startDate}
+                } else {
+                    params = {currentDate: 1};
+                }
+                const res = await this._services.getAccessStats(params);
+                let { code, data } = res;
+                // code 校验
+                if (parseInt(code, 10) !== 200) {
+                    // this.tools.message(res.message);
+                    return;
+                }
+                // data 校验
+                // if (data.statsList.length === 0) {
+                //     // console.log(data.statsList);
+                //     // 告诉父组件没有数据, 进行相应展示
+                //     this.$emit("noData");
+                //     return
+                // }
 
-            this.publishReceive.addColorStop(0, 'rgba(0, 231, 255, 0.9)')
-            this.publishReceive.addColorStop(0.5, 'rgba(0, 231, 255, 0.25)');
-            this.publishReceive.addColorStop(1, 'rgba(0, 231, 255, 0)');
+                // 图表顺序排列
+                data.statsList.sort((p1, p2) => {
+                    return p1.id - p2.id
+                });
 
-            this.publishSuccess.addColorStop(0, 'rgba(0, 0, 255, 0.9)')
-            this.publishSuccess.addColorStop(0.5, 'rgba(0, 0, 255, 0.25)');
-            this.publishSuccess.addColorStop(1, 'rgba(0, 0, 255, 0)');
+                // 准备数据容器
+                let tempAccessStatsObj = {
+                    "receiveList": [],
+                    "successList": [],
+                    "dateList": []
+                };
+                //
+                let {receiveList, successList, dateList} = tempAccessStatsObj;
+                data.statsList.forEach((item) => {
+                    receiveList.push(item.publishReceiveTotal);
+                    successList.push(item.publishSuccessTotal);
+                    dateList.push(item.date)
+                });
 
-            this.renderChart({
-                labels: ['2020-04-16', '2020-04-17', '2020-04-18', '2020-04-19', '2020-04-20', '2020-04-21', '2020-04-22', '2020-04-23', '2020-04-24', '2020-04-25'],
-                datasets: [
-                    {
-                        label: 'publish-receive',
-                        borderColor: 'rgba(250, 250, 210)',
-                        pointBackgroundColor: 'white',
-                        borderWidth: 1,
-                        pointBorderColor: 'white',
-                        backgroundColor: this.publishReceive,
-                        data: [40, 39, 10, 40, 39, 400, 40, 200, 200, 200]
-                    }, {
-                        label: 'publish-success',
-                        // backgroundColor: 'skyblue',
-                        borderColor: 'rgb(0, 0, 255)',
-                        pointBackgroundColor: 'white',
-                        pointBorderColor: 'white',
-                        borderWidth: 1,
-                        backgroundColor: this.publishSuccess,
-                        data: [60, 55, 32, 10, 2, 12, 53, 200, 300, 100]
-                    }
-                ]
-            }, {
-                responsive: true,  /*响应式*/
-                maintainAspectRatio: false  /*保持长宽比*/
-            })
+                // 画图
+                this.publishReceiveLine = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
+                this.publishSuccessLine = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450);
+
+                this.publishReceiveLine.addColorStop(0, 'rgba(250, 250, 210, 0.9)')
+                this.publishReceiveLine.addColorStop(0.5, 'rgba(250, 250, 210, 0.25)');
+                this.publishReceiveLine.addColorStop(1, 'rgba(250, 250, 210, 0)');
+
+                this.publishSuccessLine.addColorStop(0, 'rgba(60, 179, 113, 0.9)')
+                this.publishSuccessLine.addColorStop(0.5, 'rgba(60, 179, 113, 0.25)');
+                this.publishSuccessLine.addColorStop(1, 'rgba(60, 179, 113, 0)');
+
+                this.renderChart({
+                    // labels: ['2020-04-16', '2020-04-17', '2020-04-18', '2020-04-19', '2020-04-20', '2020-04-21', '2020-04-22', '2020-04-23', '2020-04-24', '2020-04-25'],
+                    labels: tempAccessStatsObj.dateList,
+                    datasets: [
+                        {
+                            label: 'publish-receive',
+                            borderColor: 'rgba(250, 250, 210)',
+                            pointBackgroundColor: 'white',
+                            borderWidth: 1,
+                            pointBorderColor: 'white',
+                            backgroundColor: this.publishReceiveLine,
+                            // data: [40, 39, 10, 40, 39, 400, 40, 200, 200, 200]
+                            data: tempAccessStatsObj.receiveList
+                        }, {
+                            label: 'publish-success',
+                            // backgroundColor: 'skyblue',
+                            borderColor: 'rgb(60,179,113)',
+                            pointBackgroundColor: 'white',
+                            pointBorderColor: 'white',
+                            borderWidth: 1,
+                            backgroundColor: this.publishSuccessLine,
+                            // data: [60, 55, 32, 10, 2, 12, 53, 200, 300, 100]
+                            data: tempAccessStatsObj.successList
+                        }
+                    ]
+                }, {
+                    responsive: true,  /*响应式*/
+                    maintainAspectRatio: false  /*保持长宽比*/
+                })
+            }
+        },
+
+        watch: {
+            startDate(newStartDate) {
+                this.drawLine(newStartDate);
+            }
         }
     }
 </script>
--- codes/access_demo/src/components/publish/publishGraph.vue
+++ codes/access_demo/src/components/publish/publishGraph.vue
@@ -1,27 +1,68 @@
 <template>
     <div class="publish-graph">
 <!--        <h4>publishGraph</h4>-->
-        <line-chart />
+        <div class="date-search">
+            <date-search ref="search" />
+        </div>
+
+        <div class="line-chart">
+            <line-chart :startDate="startDate" ref="chart" />
+        </div>
     </div>
 </template>
 
 <script>
     import lineChart from "./lineChart"
+    import dateSearch from "../common/dateSearch"
+    // import noData from "../common/noData"
+
     export default {
         name: "accessGraph",
         components: {
-            lineChart
+            lineChart,
+            dateSearch
+        },
+        data() {
+            return {
+                startDate: "",
+            }
+        },
+
+        mounted() {
+            // 监听时间查询事件
+            this.$refs.search.$on("dateSearch", this.dateSearch);
+            // 监听有无图表数据
+            // this.$refs.chart.$on("noData", this.noData)
+        },
+        methods: {
+            dateSearch(startDate){
+                this.startDate = startDate
+            }
         }
     }
 </script>
 
 <style scoped>
     .publish-graph {
-        width: 90%;
+        width: 100%;
+        height: calc(100% - 68px); /* 占满100% - 标题高度 */
+
+        position: relative;
+    }
+
+    .publish-graph .date-search {
+        width: 100%;
+        height: 42px;
+        /*margin: 0 2% 20px;*/
+        margin: 0 0 20px;
+    }
+
+    .publish-graph .line-chart {
+        width: 100%;
         height: 80%;
         border-radius: 15px;
         background: #212733;
         box-shadow: 0 2px 15px rgba(25, 25, 25, 0.27);
-        margin: 82px 5% 0;
+        /*margin: 0 10px;*/
     }
 </style>
